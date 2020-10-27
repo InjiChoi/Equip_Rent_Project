77b8ad393b127ffc115f20de41405069d7fa183a
InjiChoi <chldlswl1212@gmail.com>
2020-10-27 17:28:15 +0900

    settings.py app 추가

--- EquipManage/settings.py
+++ EquipManage/settings.py
@@ -30,15 +30,21 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'manageapp',
 ]
+CUSTOM_APPS = [
+    'equipments',
+    'students',
+    'managements',
+]
+
+INSTALLED_APPS = DJANGO_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
