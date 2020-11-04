266900837c5c6728af9555f7fa7d26cf95f8b430
InjiChoi <chldlswl1212@gmail.com>
2020-11-04 23:31:37 +0900

    중간 저장

--- managements/forms.py
+++ managements/forms.py
@@ -3,16 +3,19 @@ from managements.models import RentManage
 from students.models import Student
 from equipments.models import Equipment
 
-class RentForm(forms.Form):
-        student_id = forms.CharField()
-        student_name = forms.CharField()
-        phone_number = forms.CharField()
-        email = forms.EmailField()
-        equip_id = forms.CharField()
-        equip_type = forms.CharField()
-        equip_pic = forms.ImageField()
+# class RentForm(forms.Form):
+#         student_id = forms.CharField()
+#         student_name = forms.CharField()
+#         phone_number = forms.CharField()
+#         email = forms.EmailField()
+#         equip_id = forms.CharField()
+#         equip_type = forms.CharField()
+#         equip_pic = forms.ImageField()
 
-
+class RentForm(forms.ModelForm):
+        class Meta:
+                model = RentManage
+                fields = '__all__'
 
 # class RentStudentForm(forms.ModelForm):
 #     class Meta:
--- managements/views.py
+++ managements/views.py
@@ -3,26 +3,37 @@ from .models import RentManage
 from managements.forms import RentForm
 # Create your views here.
 
+# def rent(request):
+#         if request.method == 'POST':
+#                 # student = Student.objects.get(id=pk)
+#                 rent_form = RentForm(request.POST, request.FILES)
+#                 equip_pic = request.POST['equip_pic']
+#                 print(0)
+#                 if rent_form.is_valid():
+#                         print(55)
+#                         info = RentManage(equip_pic=rent_form.data['equip_pic'])
+#                         # rent_info = rent_form.save(commit=False)
+#                         print(1)
+#                         info.save()
+#                         print(2)
+#                 else:
+#                         print(10)
+
+#                 return render(request, 'managements/rent.html', {'rent_form':rent_form} )
+#         else:
+                
+#                 print(4)
+#                 rent_form = RentForm()
+
+#                 return render(request, 'managements/rent.html', {'rent_form':rent_form})
 def rent(request):
         if request.method == 'POST':
-                # student = Student.objects.get(id=pk)
-                rent_form = RentForm(request.POST, request.FILES)
-                equip_pic = request.POST['equip_pic']
-                print(0)
+                student_id = Student.objects.get(student_id=request.POST.get('student_id'))
+                rent_form = RentForm(request.POST)
+                rent_info = rent_form.save(commit=False)
+                rent_info.student_id = student_id
                 if rent_form.is_valid():
-                        print(55)
-                        info = RentManage(equip_pic=rent_form.data['equip_pic'])
-                        # rent_info = rent_form.save(commit=False)
-                        print(1)
-                        info.save()
-                        print(2)
-                else:
-                        print(10)
+                        messages
 
-                return render(request, 'managements/rent.html', {'rent_form':rent_form} )
-        else:
-                
-                print(4)
-                rent_form = RentForm()
-
-                return render(request, 'managements/rent.html', {'rent_form':rent_form})
+def main_page(request):
+    return render(request, 'managements/main.html',{})
