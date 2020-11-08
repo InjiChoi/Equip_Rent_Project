71402c8bc4901b2da79483c541bb0431a078345f
InjiChoi <chldlswl1212@gmail.com>
2020-11-09 00:25:51 +0900

    main page 살짝 수정

--- managements/admin.py
+++ managements/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from managements.models import RentManage
 # Register your models here.
-admin.site.register(RentManage)
+@admin.register(RentManage)
+class RentManageAdmin(admin.ModelAdmin):
+    list_display=('student_id', 'name', 'phone_number', 'email',
+                    'equip_id', 'equip_type', 'equip_pic', 'return_date')
