fd2af76112395c37659139fb87c457e593565355
YOOJIYUN <ythg7753@gmail.com>
2020-11-09 23:23:43 +0900

    메세지 출력

--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
+from django.contrib import messages
 
 # Create your views here.
 def student_register(request):
@@ -9,6 +10,9 @@ def student_register(request):
         if student_form.is_valid():
             student_info = student_form.save(commit=False)
             student_info.save()
+        else :
+            messages.info(request,"중복된 사항으로 학생 등록에 실패했습니다.")
+
         return render(request, 'students/student_register.html')
     else:
         student_form = StudentForm()
