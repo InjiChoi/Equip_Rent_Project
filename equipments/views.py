bfcc7475113a726f3b96e7d6ae93818fba46f3ba
InjiChoi <chldlswl1212@gmail.com>
2020-11-23 18:36:32 +0900

    기자재 중복 검사 시작

--- equipments/views.py
+++ equipments/views.py
@@ -5,6 +5,8 @@ from managements.models import RentManage
 from students.models import Student
 from .forms import EquipForm
 from django.contrib import messages
+import json
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def equipment_register(request):
@@ -22,6 +24,24 @@ def equipment_register(request):
     else:
         equipment_form = EquipForm()
         return render(request, 'equipments/equipment_register.html', {'equipment_form':equipment_form})
+
+# 기자재 물품 번호 중복 확인
+def equipment_overlap_check(request):
+    equip_id = request.GET.get('equip_id')
+    try:
+        # 중복 검사 실패
+        equip = Equipment.objects.get(equip_id=equip_id)
+    except:
+        # 중복 검사 성공
+        equip = None
+    if equip is None:
+        overlap = "pass"
+    else:
+        overlap = "fail"
+    ctx = {'overlap':overlap}
+
+    return JsonResponse(ctx)
+
 
 def equipment_list(request):
     equipments = Equipment.objects.all()
