278408e8d8ae792ffe96ad60b178d9b18fe9f29c
InjiChoi <chldlswl1212@gmail.com>
2020-12-25 04:46:26 +0900

    rent view 수정

--- managements/views.py
+++ managements/views.py
@@ -12,48 +12,26 @@ from django.contrib import messages
 def main_page(request):
     return render(request, 'managements/main.html',{})
 
+
 # 대여 페이지
 def rent(request):
         if request.method == 'POST':
-                try : 
-                        rent_student = Student.objects.get_or_create(
-                                student_id=request.POST.get('student_id'), 
-                                name=request.POST.get('name'),
-                                phone_number=request.POST.get('phone_number'),
-                                email=request.POST.get('email'))
-                        rent_equipment = Equipment.objects.get_or_create(
-                                equip_id=request.POST.get('equip_id'),
-                                equip_type=request.POST.get('equip_type'),
-                                rent_status=True,
-                        )
-                        rent_form = RentForm(request.POST, request.FILES)
-                        # rent_student_form = RentStudentForm(request.POST)
-                        # rent_equipment_form = RentEquipmentForm(request.POST)
-                        print(0)
-                        if rent_form.is_valid() :
-                                print(55)
-                                # rent_student = rent_student_form.save()
-                                # rent_equipment_form.save()
-                                rent_info = rent_form.save(commit=False)
-                                rent_info.student = rent_student[0]
-                                # rent_info.student = Student.objects.get(id=rent_student[0]).student_id
-                                rent_info.equip = rent_equipment[0]
-                                rent_info.rent_date = timezone.now()
-                                rent_info.save()
+                rent_student_id = request.POST.get('student_id')
+                rent_equipment_id = request.POST.get('equip_id')
 
-                        else:
-                                print(10)
+                # rent_student = Student.objects.get(student_id=rent_student_id)
+                rent_student = get_object_or_404(Student, pk=rent_student_id)
+                rent_equip = get_object_or_404(Equipment, pk=rent_equipment_id)
 
-                        return redirect('managements:rent_list')
-
-                except IntegrityError:
-                        print('integrityerror')
-                        rent_form = RentForm()
-                        rent_student_form = RentStudentForm()
-                        rent_equipment_form = RentEquipmentForm()
-                        return render(request, 'managements/rent.html', {'rent_form':rent_form, 'rent_student_form':rent_student_form, 'rent_equipment_form':rent_equipment_form})
+                rent_form = RentForm(request.POST, request.FILES)
+                if rent_form.is_valid() :
+                        rent_info = rent_form.save(commit=False)
+                        rent_info.student = rent_student
+                        rent_info.equip = rent_equip
+                        rent_info.rent_date = timezone.now()
+                        rent_info.save()
+                return redirect('managements:rent_list')
         else:
-                print('get')
                 rent_form = RentForm()
                 rent_student_form = RentStudentForm()
                 rent_equipment_form = RentEquipmentForm()
@@ -63,6 +41,7 @@ def rent(request):
                         'rent_equipment_form':rent_equipment_form
                 }
                 return render(request, 'managements/rent.html', ctx)
+
 
 def rent_list(request):
         rents = RentManage.objects.all()
@@ -75,25 +54,6 @@ def rent_list(request):
 
 # 반납 페이지
 def return_(request):
-        # if request.method == 'POST':
-        #         # return_form = ReturnForm(request.POST)
-        #         # print(1)
-        #         # if return_form.is_valid():
-        #         #         print(2)
-        #         #         return_info = ReturnHistory(**return_form.cleaned_data)
-        #         #         # return_info = return_form.save(commit=False)
-        #         #         # return_info.student = request.POST.get('return_student')
-        #         #         # return_info.equip = request.POST.get('return_equip')
-        #         #         # return_info.return_date = timezone.now()
-        #         #         return_info.save
-        #         #         print(return_info)
-
-        #         #         # ctx = {
-        #         #         #         'return_info':return_info,
-        #         #         # }
-        #         #         # return render(request, 'managements/return_result.html', ctx)
-        # # else:
-        # print(return_form.return_equipment)
         if request.method =='POST':
                 equip_id = request.POST.get('equip_id')
                 student_id = request.POST.get('student_id')
@@ -117,28 +77,25 @@ def return_(request):
                 }
 
                 return render(request, 'managements/return.html', ctx)
-
-def return_info(request):
-        rents = RentManage.objects.all()
-        
-        ctx = {
-        'rents':rents
-        }
-        return render(request, 'managements/rent_list.html', ctx)
                 
 
 
-def return_result(request):
-        return render(request, 'managements/return_result.html')
+def return_result(request, pk):
+        rent_equip = Equipment.objects.get(pk=pk)
+        rent = RentManage.objects.get(equip = rent_equip)
+        rent_student = Student.objects.get(student_id=rent.student.student_id)
+        return_instance = ReturnHistory.objects.create(student=rent_student, equip=rent_equip, return_date=timezone.now())
+        rent.delete()
+        return redirect('managements:return_list')
+
 
 def return_list(request):
         returns = ReturnHistory.objects.all()
         students = Student.objects.all()
         equipments = Equipment.objects.all()
-
         ctx = {
                 'returns':returns,
                 'students':students,
                 'equipments':equipments
         }
-        return render(request, 'managements/return_list.html')
+        return render(request, 'managements/return_list.html', ctx)
