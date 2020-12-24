a7d2825ad182799f8eee58d345206144d34f680a
InjiChoi <chldlswl1212@gmail.com>
2020-12-25 06:48:20 +0900

    rent_status 수정 & 반납 alert 추가

--- managements/urls.py
+++ managements/urls.py
@@ -6,6 +6,7 @@ app_name = 'managements'
 urlpatterns = [
     path('',views.main_page, name="main_page"),
     path('rent/', views.rent, name="rent"),
+    path('rent/search_ajax/', views.rent_search_ajax, name='rent_search_ajax'),
     path('rent/rent_list', views.rent_list, name="rent_list"),
     path('return/', views.return_, name="return_"),
     path('return/return_result/<int:pk>', views.return_result, name='return_result'),
--- managements/views.py
+++ managements/views.py
@@ -29,6 +29,8 @@ def rent(request):
                         rent_info.student = rent_student
                         rent_info.equip = rent_equip
                         rent_info.rent_date = timezone.now()
+                        rent_equip.rent_status = True
+                        rent_equip.save()
                         rent_info.save()
                 return redirect('managements:rent_list')
         else:
@@ -41,6 +43,12 @@ def rent(request):
                         'rent_equipment_form':rent_equipment_form
                 }
                 return render(request, 'managements/rent.html', ctx)
+
+# 학생 정보 조회
+def rent_search_ajax(request):
+        print(1)
+        dict={'test':'json_sample'}
+        return HttpResponse(json.dumps(dict), content_type='application/json')
 
 
 def rent_list(request):
