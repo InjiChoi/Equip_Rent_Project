1e8d5c51447d634d15004404b8b284f2337b05f3
InjiChoi <chldlswl1212@gmail.com>
2021-01-10 00:13:30 +0900

    user login 완료

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from .models import Staff
 from django.contrib.auth.models import User
 from django.contrib import auth
 
 class StaffForm(forms.ModelForm):
     class Meta:
-        model = Staff
+        model = User
         fields = ('username','password',)
--- users/models.py
+++ users/models.py
@@ -2,7 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Staff(User):
-    nickname = models.CharField(max_length=30)
-
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        proxy = True
