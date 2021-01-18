afb796615996bc6d8d22905e44ec1432570c1782
InjiChoi <chldlswl1212@gmail.com>
2021-01-19 01:33:03 +0900

    기자재 별 파이 차트 페이지 추가

--- graphs/urls.py
+++ graphs/urls.py
@@ -5,4 +5,7 @@ app_name = 'graphs'
 
 urlpatterns = [
     path('', views.graph_main, name='graph_main'),
+    path('laptop/', views.graph_laptop, name='graph_laptop'),
+    path('tablet/', views.graph_tablet, name='graph_tablet'),
+    path('sensor/', views.graph_sensor, name='graph_sensor'),
 ]
