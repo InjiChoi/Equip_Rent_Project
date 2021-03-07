a1a506eba2f294e8346f25faa7741f511608c5fc
InjiChoi <chldlswl1212@gmail.com>
2021-03-07 21:40:06 +0900

    [fix]receivecode.html 생성 & sendmail, createmessage 함수 수정중

--- EquipManage/urls.py
+++ EquipManage/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from managements import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('student/', include('students.urls')),
     path('users/', include('users.urls')),
     path('graphs/', include('graphs.urls')),
+    path('receivecode.html/', views.receivecode, name='receivecode'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- managements/views.py
+++ managements/views.py
@@ -24,6 +24,19 @@ from django.contrib.auth.decorators import login_required
 import xlwt
 from datetime import datetime
 
+import base64
+import smtplib
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+import requests
+import json
+import sys
+
 @login_required(login_url='/users/')
 def main_page(request):
     return render(request, 'managements/main.html',{})
@@ -74,9 +87,10 @@ def rent(request):
                                 'domain': current_site.domain,
                         })
                         mail_title = "숙명여자대학교 IT공학과 기자재 대여 서약서"
-                        email = EmailMultiAlternatives(subject = mail_title, body=html_message, to=[rent_student.email])
-                        email.content_subtype = 'html'
-                        email.send()
+                        # email = EmailMultiAlternatives(subject = mail_title, body=html_message, to=[rent_student.email])
+                        # email.content_subtype = 'html'
+                        # email.send()
+                        sendmail(rent_student.email, mail_title, html_message)
                 return redirect('managements:rent_list')
         else:
                 rent_form = RentForm()
@@ -625,3 +639,83 @@ def search_pending_list(request):
         }
 
         return render(request, 'managements/pending_list_search.html', ctx)   
+
+
+# gmail api receivecode
+def receivecode(request):
+        return render(request, 'managements/receivecode.html')
+
+
+# ----------------------------------------------------------------------------------------- 
+def CreateMessage(sender, to, subject, message_text=None, attachment_path=None): 
+        """Create a message for an email. 
+        Args: sender: Email address of the sender. 
+        to: Email address of the receiver. 
+        subject: The subject of the email message. 
+        message_text: The text of the email message. 
+        attachment_path: Full path for attachment (dir+filename) 
+        Returns: An object containing a base64url encoded email object. """ 
+        message = MIMEMultipart() 
+        message['to'] = to 
+        message['from'] = sender 
+        message['subject'] = subject 
+        #message['cc'] = cc 
+        #message['bcc'] = cc 
+        #msg.add_header('reply-to', return_addr) 
+        
+        if message_text != None: 
+                msg = MIMEText(message_text, 'html') 
+                message.attach(msg) 
+                
+        if attachment_path != None: 
+                content_type, encoding = mimetypes.guess_type(attachment_path) 
+                
+                if content_type is None or encoding is not None: 
+                        content_type = 'application/octet-stream' 
+                main_type, sub_type = content_type.split('/', 1) 
+                if main_type == 'text': 
+                        fp = open(attachment_path, 'rb') 
+                        msg = MIMEText(fp.read().decode('utf8'), _subtype=sub_type) 
+                        fp.close() 
+                elif main_type == 'image': 
+                        fp = open(attachment_path, 'rb') 
+                        msg = MIMEImage(fp.read(), _subtype=sub_type) 
+                        fp.close() 
+                elif main_type == 'audio': 
+                        fp = open(attachment_path, 'rb') 
+                        msg = MIMEAudio(fp.read(), _subtype=sub_type) 
+                        fp.close() 
+                else: 
+                        fp = open(attachment_path, 'rb') 
+                        msg = MIMEBase(main_type, sub_type) 
+                        msg.set_payload(fp.read()) 
+                        fp.close()
+
+                msg.add_header('Content-Disposition', 'attachment', filename=os.path.basename(attachment_path)) 
+                message.attach(msg) 
+                
+                try: 
+                        return {'raw': base64.urlsafe_b64encode(message.as_string())} 
+                except: 
+                        return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode('utf8')} 
+# ----------------------------------------------------------------------------------------- 
+def sendmail(to, subject, message_text_html, attachment=None): 
+        # Your access code 
+        access_token = 'ya29.a0AfH6SMDa5bjmbrDipgfCrffpLkCUDl2gjlePejn7eAQ9b0elT2dVhhXfllhXuYL4b1I7oTUgRWs1_mvUiYCobm9wJm8Va0pD67Hom9xaqWtbN34cTQtTswN1z_GUwQ7ONm0TTiYGCJUe2OwSFlzCvcOnW0YH' 
+        user_id = 'chldlswl1212@gmail.com'
+        
+        URL = 'https://www.googleapis.com/gmail/v1/users/' + user_id + '/messages/send' 
+        request_header = { "Content-Type": "application/json", "Authorization": "Bearer " + access_token, "X-GFE-SSL": "yes" } 
+        payload = CreateMessage(user_id, to, subject, message_text_html, attachment) 
+        # validate payload on https://developers.google.com/gmail/api/v1/reference/users/messages/send 
+        # print (payload) 
+        # exit(1) 
+
+        response = requests.post(URL, headers=request_header, data=json.dumps(payload)) 
+        
+        print ('*** Error *** : sendmail fail') 
+        print(response) 
+        print(response.text) 
+        # ----------------------------------------------------------------------------------------- 
+        if __name__ == "__main__":
+                sendmail('vnavna16@naver.com', 'subject_test', 'message_test', None)
